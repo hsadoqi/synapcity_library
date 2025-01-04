c4c5dc70dbe40747bce3e139d87097a7399b5142
Hanaa Sadoqi <hsadoqi@gmail.com>
2025-01-04 02:36:07 -0500

    build: install dependencies and configure `cypress`, `tsconfig` and `jest`

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,15 +1,13 @@
-// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
-
 /**
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
 const config = {
-    trailingComma: 'all',
-    bracketSpacing: true,
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
-}
+	trailingComma: "all",
+	bracketSpacing: true,
+	tabWidth: 4,
+	semi: false,
+	singleQuote: true,
+};
 
-export default config
+export default config;
