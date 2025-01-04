ec7f97869376aa4eb62ec6626cfb4f173eb5f1ab
Hanaa Sadoqi <hsadoqi@gmail.com>
2025-01-04 15:21:55 -0500

    fix: remove accidentally duplicated project folders and files

--- para-synapcity/src/app/providers.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-
-import { ThemeProvider } from '@/contexts'
-export default function Providers({ children }: { children: React.ReactNode }) {
-    return <ThemeProvider>{children}</ThemeProvider>
-}
