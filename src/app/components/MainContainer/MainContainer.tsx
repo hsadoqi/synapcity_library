3178f41c5c8766956d027aeddc2ae3a966284071
Hanaa Sadoqi <hsadoqi@gmail.com>
2025-01-05 18:37:08 -0500

    chore: set up basic layout containers and styles

--- src/app/components/MainContainer/MainContainer.tsx
+++ src/app/components/MainContainer/MainContainer.tsx
@@ -1,3 +1,23 @@
-export default function MainContainer() {
-    return <>Main Container</>
+import styles from './MainContainer.module.css'
+
+import { MainHeader as Header } from '../MainHeader'
+import clsx from 'clsx'
+
+export default function MainContainer({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <div className="full-screen-container">
+            <Header />
+            <div className={styles.mainContainer}>
+                <div
+                    className={clsx(styles.scrollYContainer, 'max-w-7xl py-20')}
+                >
+                    {children}
+                </div>
+            </div>
+        </div>
+    )
 }
--- src/app/components/MainHeader/MainHeader.tsx
+++ src/app/components/MainHeader/MainHeader.tsx
@@ -1,3 +1,14 @@
+import styles from './MainHeader.module.css'
+import clsx from 'clsx'
+
 export default function MainHeader() {
-    return <>Main Header</>
+    return (
+        <header className={clsx(styles.headerContainer)}>
+            <div className={clsx(styles.hoverableContainer, 'group')}>
+                <div className={clsx(styles.header, 'group-hover:visible')}>
+                    Header
+                </div>
+            </div>
+        </header>
+    )
 }
