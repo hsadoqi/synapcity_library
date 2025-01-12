0fe66a9b5b8ee194b2c9c245514b439e21f481a3
Hanaa Sadoqi <hsadoqi@gmail.com>
2025-01-12 04:29:54 -0500

    refactor: update utils and types for styling; organize files

---
+++ src/types/styling.ts
@@ -0,0 +1,22 @@
+export enum Color {
+    Green = 'green',
+    Neutral = 'neutral',
+    Blue = 'blue',
+    Sky = 'sky',
+    Purple = 'purple',
+    Red = 'red',
+    Pink = 'pink',
+}
+
+export type ColorShade =
+    | 50
+    | 100
+    | 200
+    | 300
+    | 400
+    | 500
+    | 600
+    | 700
+    | 800
+    | 900
+    | 950
