437c0b03e57274c8f990e0b8cd9519bc1bf87628
Hanaa Sadoqi <hsadoqi@gmail.com>
2025-01-31 00:15:47 -0500

    fix: update library and notebook data; add subscriptions between stores

--- src/stores/notebook-store.tsx
+++ src/stores/notebook-store.tsx
@@ -1,5 +1,7 @@
 import { createStore, StoreApi, useStore } from 'zustand'
 import { Notebook, Note } from '@/types/libraryData'
+import { subscribeWithSelector } from 'zustand/middleware'
+import { libraryStore } from './library-store'
 
 export type DefaultNotebookType = Notebook & { default: boolean }
 
@@ -28,7 +30,8 @@ export type NotebookState = {
 }
 
 export type NotebookActions = {
-    setNotebook: (notebook: Notebook) => void
+    setNotebook: (notebook: Notebook | null) => void
+    setActiveNotebookById: (noteBookId: string) => void
     loadNotebooks: (notebooks: Notebook[]) => void
     filterNotebooks: (searchQuery: string) => void
     resetFilters: () => void
@@ -40,77 +43,94 @@ export type NotebookActions = {
 
 export type NotebookStore = NotebookState & NotebookActions
 
-export const notebookStore = createStore<NotebookStore>()((set) => ({
-    notebooks: [defaultNotebook],
-    filteredNotebooks: [defaultNotebook],
-    selectedNotebook: null,
-    defaultNotebook,
-    setNotebook: (notebook: Notebook) => set({ selectedNotebook: notebook }),
-    loadNotebooks: (notebooks) =>
-        set({ notebooks, filteredNotebooks: notebooks }),
-    filterNotebooks: (searchQuery: string) =>
-        set((state) => ({
-            filteredNotebooks: state.notebooks.filter((notebook) =>
-                notebook.name.toLowerCase().includes(searchQuery.toLowerCase()),
-            ),
-        })),
-    resetFilters: () =>
-        set((state) => ({ filteredNotebooks: state.notebooks })),
-    addNotebook: (notebook: Notebook) =>
-        set((state) => ({ notebooks: [...state.notebooks, notebook] })),
-    archiveNotebook: () =>
-        set((state) => {
-            if (state.selectedNotebook) {
-                return {
-                    notebooks: state.notebooks.map((notebook) =>
-                        notebook.id === state.selectedNotebook?.id
-                            ? { ...notebook, isArchived: true }
-                            : notebook,
-                    ),
-                    selectedNotebook: {
+export const notebookStore = createStore<NotebookStore>()(
+    subscribeWithSelector((set) => ({
+        notebooks: [defaultNotebook],
+        filteredNotebooks: [defaultNotebook],
+        selectedNotebook: null,
+        defaultNotebook,
+        setNotebook: (notebook: Notebook | null) =>
+            set({ selectedNotebook: notebook }),
+        setActiveNotebookById: (notebookId: string) => {
+            set((state) => {
+                if (state.notebooks) {
+                    const foundNotebook = state.notebooks.find(
+                        (notebook) => notebook.id === notebookId,
+                    )
+                    return { selectedNotebook: foundNotebook }
+                } else {
+                    return { selectedNotebook: defaultNotebook }
+                }
+            })
+        },
+        loadNotebooks: (notebooks) =>
+            set({ notebooks, filteredNotebooks: notebooks }),
+        filterNotebooks: (searchQuery: string) =>
+            set((state) => ({
+                filteredNotebooks: state.notebooks.filter((notebook) =>
+                    notebook.name
+                        .toLowerCase()
+                        .includes(searchQuery.toLowerCase()),
+                ),
+            })),
+        resetFilters: () =>
+            set((state) => ({ filteredNotebooks: state.notebooks })),
+        addNotebook: (notebook: Notebook) =>
+            set((state) => ({ notebooks: [...state.notebooks, notebook] })),
+        archiveNotebook: () =>
+            set((state) => {
+                if (state.selectedNotebook) {
+                    return {
+                        notebooks: state.notebooks.map((notebook) =>
+                            notebook.id === state.selectedNotebook?.id
+                                ? { ...notebook, isArchived: true }
+                                : notebook,
+                        ),
+                        selectedNotebook: {
+                            ...state.selectedNotebook,
+                            isArchived: true,
+                        },
+                    }
+                }
+                return state
+            }),
+        restoreNotebook: () =>
+            set((state) => {
+                if (state.selectedNotebook) {
+                    return {
+                        notebooks: state.notebooks.map((notebook) =>
+                            notebook.id === state.selectedNotebook?.id
+                                ? { ...notebook, isArchived: false }
+                                : notebook,
+                        ),
+                        selectedNotebook: {
+                            ...state.selectedNotebook,
+                            isArchived: false,
+                        },
+                    }
+                }
+                return state
+            }),
+        updateNotebook: (updates: Partial<Notebook>) =>
+            set((state) => {
+                if (state.selectedNotebook) {
+                    const updatedNotebook = {
                         ...state.selectedNotebook,
-                        isArchived: true,
-                    },
+                        ...updates,
+                    }
+                    return {
+                        notebooks: state.notebooks.map((notebook) =>
+                            notebook.id === updatedNotebook.id
+                                ? updatedNotebook
+                                : notebook,
+                        ),
+                        selectedNotebook: updatedNotebook,
+                    }
                 }
-            }
-            return state
-        }),
-    restoreNotebook: () =>
-        set((state) => {
-            if (state.selectedNotebook) {
-                return {
-                    notebooks: state.notebooks.map((notebook) =>
-                        notebook.id === state.selectedNotebook?.id
-                            ? { ...notebook, isArchived: false }
-                            : notebook,
-                    ),
-                    selectedNotebook: {
-                        ...state.selectedNotebook,
-                        isArchived: false,
-                    },
-                }
-            }
-            return state
-        }),
-    updateNotebook: (updates: Partial<Notebook>) =>
-        set((state) => {
-            if (state.selectedNotebook) {
-                const updatedNotebook = {
-                    ...state.selectedNotebook,
-                    ...updates,
-                }
-                return {
-                    notebooks: state.notebooks.map((notebook) =>
-                        notebook.id === updatedNotebook.id
-                            ? updatedNotebook
-                            : notebook,
-                    ),
-                    selectedNotebook: updatedNotebook,
-                }
-            }
-            return state
-        }),
-}))
+                return state
+            }),
+    })),
+)
 
 const createBoundedUseStore = ((store) => (selector) =>
     useStore(store, selector)) as <S extends StoreApi<unknown>>(
@@ -123,3 +143,19 @@ const createBoundedUseStore = ((store) => (selector) =>
 type ExtractState<S> = S extends { getState: () => infer X } ? X : never
 
 export const useNotebookStore = createBoundedUseStore(notebookStore)
+
+notebookStore.subscribe(
+    (state) => state.selectedNotebook,
+    (selectedNotebook) => {
+        if (selectedNotebook && selectedNotebook.libraryId) {
+            const { setActiveLibraryById, selectedLibrary } =
+                libraryStore.getState()
+            if (
+                !selectedLibrary ||
+                !selectedLibrary.notebooks.includes(selectedNotebook)
+            ) {
+                setActiveLibraryById(selectedNotebook.libraryId)
+            }
+        }
+    },
+)
